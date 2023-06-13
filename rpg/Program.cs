48b95d14bc3abf2c5068b55a1a8adbf8f8021a70
Caio Fernandes <caio.fernandes.1216303@sga.pucminas.br>
2023-06-13 11:27:30 -0300

    Melhora os testes no program

--- rpg/Program.cs
+++ rpg/Program.cs
@@ -3,8 +3,8 @@ Pessoa p = new Pessoa();
 Monstro m = new Monstro();
 Animal a = new Animal();
 
-Console.WriteLine(p.getNome());
-Console.WriteLine(m.getNome());
-Console.WriteLine(a.getNome());
+Console.WriteLine($"Nome da pessoa: {p.getNome()}\nRaça: {p.getRaca().getNome()}\nClasse: {p.getClasse().getNome()}\nAtaque: {p.getAtaque()}\nDefesa: {p.getDefesa()}\nSorte: {p.getSorte()}\nNível: {p.getNivel()}\nVida: {p.getVida()}\nMana: {p.getMana()}\nDinheiro: {p.getDinheiro()}\nItem ativo: {p.getItemAtivo()}\nAtaques: {p.getAtaques()}\nItens: {p.getItens()}");
+Console.WriteLine($"\n\nMonstro: {m.getNome()}");
+Console.WriteLine($"\n\nAnimal: {a.getNome()}");
 
 // REVISAR AS LINHAS 27 A 30 DE PERSONAGEM.CS
