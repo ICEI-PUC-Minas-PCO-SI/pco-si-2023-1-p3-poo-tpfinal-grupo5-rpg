738279e55a25e4f0fd7c82546f0117d6d2885499
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2023-06-16 21:55:42 -0300

    Adicionando componentes

--- telas/tela-game/Form1.Designer.cs
+++ telas/tela-game/Form1.Designer.cs
@@ -28,18 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            button2 = new Button();
+            button1 = new Button();
+            button3 = new Button();
+            panel2 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 11);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(137, 127);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(23, 26, 33);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Location = new Point(0, 210);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(922, 150);
+            panel1.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(22, 24);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(110, 100);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(141, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(728, 28);
+            label1.TabIndex = 3;
+            label1.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rutrum magna.";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(407, 90);
+            button2.Name = "button2";
+            button2.Size = new Size(193, 34);
+            button2.TabIndex = 5;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(180, 90);
+            button1.Name = "button1";
+            button1.Size = new Size(193, 34);
+            button1.TabIndex = 6;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(635, 90);
+            button3.Name = "button3";
+            button3.Size = new Size(193, 34);
+            button3.TabIndex = 7;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(23, 26, 33);
+            panel2.Location = new Point(0, 361);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(922, 137);
+            panel2.TabIndex = 8;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
             ClientSize = new Size(920, 496);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Button button3;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private Panel panel2;
     }
 }
