82f3b53bb5942fab013ce24ea08cfb951811b4d1
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2023-06-18 16:15:43 -0300

    Removendo botão de créditos

--- telas/tela-inicial/Form1.Designer.cs
+++ telas/tela-inicial/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             button1 = new Button();
-            button2 = new Button();
             button3 = new Button();
             pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
@@ -46,27 +45,12 @@
             button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Trebuchet MS", 11F, FontStyle.Bold, GraphicsUnit.Point);
             button1.ForeColor = Color.FromArgb(247, 247, 247);
-            button1.Location = new Point(343, 302);
+            button1.Location = new Point(343, 318);
             button1.Name = "button1";
             button1.Size = new Size(281, 41);
             button1.TabIndex = 1;
             button1.Text = "INICIAR JOGO";
             button1.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.FromArgb(23, 26, 33);
-            button2.FlatAppearance.BorderColor = Color.FromArgb(22, 22, 22);
-            button2.FlatAppearance.BorderSize = 2;
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Font = new Font("Trebuchet MS", 11F, FontStyle.Bold, GraphicsUnit.Point);
-            button2.ForeColor = Color.FromArgb(247, 247, 247);
-            button2.Location = new Point(343, 355);
-            button2.Name = "button2";
-            button2.Size = new Size(281, 41);
-            button2.TabIndex = 2;
-            button2.Text = "CRÉDITOS";
-            button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
@@ -76,7 +60,7 @@
             button3.FlatStyle = FlatStyle.Flat;
             button3.Font = new Font("Trebuchet MS", 11F, FontStyle.Bold, GraphicsUnit.Point);
             button3.ForeColor = Color.FromArgb(247, 247, 247);
-            button3.Location = new Point(343, 408);
+            button3.Location = new Point(343, 378);
             button3.Name = "button3";
             button3.Size = new Size(281, 41);
             button3.TabIndex = 3;
@@ -110,7 +94,6 @@
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             ClientSize = new Size(920, 496);
             Controls.Add(button3);
-            Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(pictureBox1);
             Controls.Add(pictureBox2);
@@ -125,7 +108,6 @@
         #endregion
 
         private Button button1;
-        private Button button2;
         private Button button3;
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
