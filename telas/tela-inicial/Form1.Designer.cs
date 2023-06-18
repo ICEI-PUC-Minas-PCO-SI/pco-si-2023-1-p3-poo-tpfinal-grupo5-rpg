1199a0a45f40c55e6cc9ee6f3106682a19c890d9
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2023-06-18 15:15:53 -0300

    Alterando tela inicial

--- telas/tela-inicial/Form1.Designer.cs
+++ telas/tela-inicial/Form1.Designer.cs
@@ -33,74 +33,92 @@
             button2 = new Button();
             button3 = new Button();
             pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.BackColor = Color.FromArgb(204, 22, 38);
-            button1.FlatAppearance.BorderColor = Color.FromArgb(247, 247, 247);
+            button1.BackColor = Color.FromArgb(23, 26, 33);
+            button1.FlatAppearance.BorderColor = Color.FromArgb(22, 22, 22);
+            button1.FlatAppearance.BorderSize = 2;
             button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Tahoma", 11F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.ForeColor = Color.Black;
-            button1.Location = new Point(139, 107);
+            button1.Font = new Font("Trebuchet MS", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.FromArgb(247, 247, 247);
+            button1.Location = new Point(343, 302);
             button1.Name = "button1";
-            button1.Size = new Size(254, 52);
-            button1.TabIndex = 0;
+            button1.Size = new Size(281, 41);
+            button1.TabIndex = 1;
             button1.Text = "INICIAR JOGO";
             button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            button2.BackColor = Color.FromArgb(204, 22, 38);
-            button2.FlatAppearance.BorderColor = Color.FromArgb(247, 247, 247);
+            button2.BackColor = Color.FromArgb(23, 26, 33);
+            button2.FlatAppearance.BorderColor = Color.FromArgb(22, 22, 22);
+            button2.FlatAppearance.BorderSize = 2;
             button2.FlatStyle = FlatStyle.Flat;
-            button2.Font = new Font("Tahoma", 11F, FontStyle.Bold, GraphicsUnit.Point);
-            button2.ForeColor = Color.Black;
-            button2.Location = new Point(139, 188);
+            button2.Font = new Font("Trebuchet MS", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.FromArgb(247, 247, 247);
+            button2.Location = new Point(343, 355);
             button2.Name = "button2";
-            button2.Size = new Size(254, 52);
-            button2.TabIndex = 4;
+            button2.Size = new Size(281, 41);
+            button2.TabIndex = 2;
             button2.Text = "CRÉDITOS";
             button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
-            button3.BackColor = Color.FromArgb(204, 22, 38);
-            button3.FlatAppearance.BorderColor = Color.FromArgb(247, 247, 247);
+            button3.BackColor = Color.FromArgb(23, 26, 33);
+            button3.FlatAppearance.BorderColor = Color.FromArgb(22, 22, 22);
+            button3.FlatAppearance.BorderSize = 2;
             button3.FlatStyle = FlatStyle.Flat;
-            button3.Font = new Font("Tahoma", 11F, FontStyle.Bold, GraphicsUnit.Point);
-            button3.ForeColor = Color.Black;
-            button3.Location = new Point(139, 269);
+            button3.Font = new Font("Trebuchet MS", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = Color.FromArgb(247, 247, 247);
+            button3.Location = new Point(343, 408);
             button3.Name = "button3";
-            button3.Size = new Size(254, 52);
-            button3.TabIndex = 5;
+            button3.Size = new Size(281, 41);
+            button3.TabIndex = 3;
             button3.Text = "SAIR";
             button3.UseVisualStyleBackColor = false;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(321, 12);
+            pictureBox1.Location = new Point(302, 21);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(462, 472);
-            pictureBox1.TabIndex = 6;
+            pictureBox1.Size = new Size(366, 285);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 4;
             pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(302, 302);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(366, 175);
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Black;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             ClientSize = new Size(920, 496);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(pictureBox1);
+            Controls.Add(pictureBox2);
             ForeColor = Color.Black;
             FormBorderStyle = FormBorderStyle.FixedDialog;
             Name = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
         }
 
@@ -110,5 +128,6 @@
         private Button button2;
         private Button button3;
         private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
     }
 }
