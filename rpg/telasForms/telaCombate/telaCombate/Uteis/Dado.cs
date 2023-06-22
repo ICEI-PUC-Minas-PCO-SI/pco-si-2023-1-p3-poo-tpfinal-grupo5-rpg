e07e569164db5753f307a1a52bf4663a1469b30c
Caio Fernandes <caio.fernandes.1216303@sga.pucminas.br>
2023-06-22 10:24:06 -0300

    Corrige o Dado

--- rpg/telasForms/telaCombate/telaCombate/Uteis/Dado.cs
+++ rpg/telasForms/telaCombate/telaCombate/Uteis/Dado.cs
@@ -4,6 +4,11 @@ public static class Dado
 {
     public static int RandomNumber(int minValue, int maxValue)
     {
+        if(minValue < maxValue){
+            int temp = minValue;
+            minValue = maxValue;
+            maxValue = temp;
+        }
         Random random = new Random();
         return random.Next(minValue, maxValue + 1);
     }
